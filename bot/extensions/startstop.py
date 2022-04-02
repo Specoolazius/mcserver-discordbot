42904399e9493f17ad8f7d3b7a70155e5f8f7eae
Specoolazius <sweetgreen@freenet.de>
2022-04-02 21:03:31 +0200

    empty start stops

---
+++ bot/extensions/startstop.py
@@ -0,0 +1,18 @@
+import discord
+from discord.commands import slash_command
+
+from libs import Client
+
+
+class StartStop(discord.Cog):
+    """< discord.Cog >
+
+    Extension for starting and stopping the Minecraft server.
+    """
+
+    def __init__(self, bot: Client):
+        self.bot = bot
+
+    @slash_command(name='start')
+    async def __execute_start(self, ctx: discord.ApplicationContext) -> None:
+        pass
